694b01a245f836cfc65a574a2ac9382d9b292ca9
HeroGamerGabby <filippopietro.neri@gmail.com>
2025-02-26 12:23:12 +0000

    Punto 2

---
+++ neri-filippo-fila-a/src/app/fourth/fourth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth',
+  imports: [],
+  templateUrl: './fourth.component.html',
+  styleUrl: './fourth.component.css'
+})
+export class FourthComponent {
+
+}
