6cfa09b54d3157015a8b9b5228a56438598e77e1
HeroGamerGabby <filippopietro.neri@gmail.com>
2025-02-26 12:52:07 +0000

    Punto 6

--- neri-filippo-fila-a/src/app/app.component.ts
+++ neri-filippo-fila-a/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SecondComponent } from './second/second.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SecondComponent],
+  imports: [SecondComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
